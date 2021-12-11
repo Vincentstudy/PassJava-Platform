84ffa94ce7da913bff128808c92418c6c2943d8f
zqy <632868671@qq.com>
2021-12-11 11:12:23 +0800

    实现架构

---
+++ passjava-question/src/test/java/com/vincentpassjava/passjavaquestion/PassjavaQuestionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vincentpassjava.passjavaquestion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassjavaQuestionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
